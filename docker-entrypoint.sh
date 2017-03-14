a38c10d5a6cc42ca740c7c639291c70287cfcd41
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2017-03-14 15:19:31 -0400

    Entrypoints

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,59 +1,34 @@
 #!/usr/bin/env bash
 set -eo pipefail
+shopt -s nullglob
 
 source /python/bin/activate
 
-echo -n "* Check folder permissions"
-find /python /app ! -user app -exec chown app:app {} \;
-echo " [Done]"
-
-update-python-env() {
-    if [ -f /app/requirements.txt ]; then
-    echo -n "* Installing packages from /app/requirements.txt ... "
-    gosu app pip install --quiet -r /requirements.txt
-    echo "[Done]"
-    fi
-
-    if [ -f /app/setup.py ]; then
-    echo -n "* Installing python package found in /app ..."
-    gosu app pip install --quiet -e /app
-    echo "[Done]"
-    fi
-
-    echo "* Python packages installed: $(pip freeze | wc -l)"
-}
-
-run-entrypoints() {
-    test -z "$@" && exit 1;
-
-    # Loading optional entrypoints scripts.
-    for f in "$@"; do
-        case "$f" in
-            *.sh)
-                echo "\n==== $0: Running $f \n====\n";
-                gosu app bash "$f" ;
-                echo -e "\n==== Completed $f ====\n"
-                ;;
-
-            *.py)
-                echo "\n==== $0: Running $f \n====\n";
-                gosu app python "$f" ;
-                echo -e "\n==== Completed $f ====\n"
-                ;;
-
-            *) echo "$0: Ignoring $f" ;;
-        esac
-    done
-}
-
-export -f update-python-env
-
-run-entrypoints /docker-entrypoint.d/pre-*
-
 case "$1" in
-    python|uwsgi|-)
-        update-python-env
-        run-entrypoints /docker-entrypoint.d/post-*
+    run-entrypoints)
+        test -z "$2" && exit 1;
+
+        # Loading optional entrypoints scripts.
+        for f in $2; do
+            case "$f" in
+                *.sh)
+                    echo "\n==== $0: Running $f \n====\n";
+                    gosu app bash "$f" ;
+                    echo -e "\n==== Completed $f ====\n"
+                    ;;
+
+                *.py)
+                    echo "\n==== $0: Running $f \n====\n";
+                    gosu app python "$f" ;
+                    echo -e "\n==== Completed $f ====\n"
+                    ;;
+
+                *) echo "$0: Ignoring $f" ;;
+            esac
+        done
+    ;;
+    python|python3|uwsgi|-)
+        gosu app "$0" run-entrypoints /docker-entrypoint.d/post-*
 
         # Cleanup shortcut
         if [ ${1} = '-' ]; then
@@ -63,6 +38,8 @@ case "$1" in
         set -- gosu app "$@"
         ;;
 esac
+
+"$0" run-entrypoints /docker-entrypoint.d/pre-*
 
 echo "running: $@"
 exec "$@"
